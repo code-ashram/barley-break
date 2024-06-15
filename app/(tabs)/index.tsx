4a0115177b3e79c857d24bde9bf5f4015f32e7d0
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-06-15 05:27:10 +0300

    add first markup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,146 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Image, StyleSheet, Platform} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
+import {HelloWave} from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+import {Colors} from "@/constants/Colors";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Barley Break</ThemedText>
+
+      <ThemedView style={styles.board}>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            1
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            2
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            3
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            4
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            5
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            6
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            7
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            8
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            9
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            10
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            11
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            13
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            13
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            14
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.tile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+            15
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.emptyTile}>
+          <ThemedText style={styles.tileNumber} type='subtitle'>
+
+          </ThemedText>
+        </ThemedView>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+
+      <ThemedView style={styles.steps}>
+        <ThemedText type="title">Your steps: 12</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+
+    </ThemedView>
+  )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    paddingTop: 50,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  board: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 20,
+    width: 360,
+    height: 360,
+    borderWidth: 2,
+    borderColor: 'wheat',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  tile: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 89,
+    height: 89,
+    borderWidth: 1,
+    borderColor: 'white',
   },
+  emptyTile: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3a3c3e',
+    width: 89,
+    height: 89,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  tileNumber: {
+    fontSize: 40,
+  },
+  steps: {
+    marginTop: 30
+  }
 });
