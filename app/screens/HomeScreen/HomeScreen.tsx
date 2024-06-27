d6b7bc0ef3300d7c1ea2281b3e5dfcacd746b598
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-06-27 22:44:21 +0300

    add steps and images

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,24 +1,32 @@
-import {useMemo, useState} from "react";
-import {Tile} from "@/models";
-import {ThemedView} from "@/components/ThemedView";
-import {ThemedText} from "@/components/ThemedText";
-import {Button} from "react-native-paper";
-import {Ionicons} from "@expo/vector-icons";
-import TileBar from "@/components/Tile";
-import {StyleSheet} from "react-native";
-import {canMove, checkBoard, generateNewBoard, replaceTiles} from "@/app/screens/HomeScreen/utils";
+import { useMemo, useState } from 'react'
+import { StyleSheet } from 'react-native'
+import { Button } from 'react-native-paper'
+import { Image } from 'expo-image'
+
+import TileBar from '@/components/Tile'
+import { ThemedView } from '@/components/ThemedView'
+import { ThemedText } from '@/components/ThemedText'
+
+import { Tile } from '@/models'
+import { canMove, checkBoard, generateNewBoard, replaceTiles } from '@/app/screens/HomeScreen/utils'
+import { Ionicons } from '@expo/vector-icons'
 
 const HomeScreen = () => {
   const [board, setBoard] = useState<Tile[]>(generateNewBoard())
+  const [step, setStep] = useState<number>(0)
 
   const handleResetBoard = () => {
     setBoard(generateNewBoard())
+    setStep(0)
   }
 
   const handleSwapTiles = (target: Tile): void => {
     const source = board.find((tile) => tile.id === 0)
 
-    if (canMove(source, target)) setBoard(replaceTiles(board, source, target))
+    if (canMove(source, target)) {
+      setBoard(replaceTiles(board, source, target))
+      setStep((prevStep) => prevStep + 1)
+    }
   }
 
   const isComplete = useMemo(() => checkBoard(board), [board])
@@ -28,20 +36,33 @@ const HomeScreen = () => {
       <ThemedText type="title">Barley Break</ThemedText>
 
       <ThemedView style={styles.steps}>
-        <ThemedText type="title">Your steps: 0</ThemedText>
+        <ThemedText type="title">Your steps: {step}</ThemedText>
 
         <Button mode="contained" buttonColor="#336edc" onPress={handleResetBoard}>
-          <Ionicons name="reload" size={22} color="white"/>
+          <Ionicons name="reload" size={22} color="white" />
         </Button>
       </ThemedView>
 
       <ThemedView style={styles.board}>
         {board.map((tile) =>
-          <TileBar key={tile.id} tile={tile} onTap={handleSwapTiles}/>
+          <TileBar key={tile.id} tile={tile} onTap={handleSwapTiles} />
         )}
       </ThemedView>
 
-      {isComplete && <ThemedText>You are winner!</ThemedText>}
+      {isComplete &&
+        <ThemedView>
+          <ThemedText style={styles.winnerTitle}>You are winner!</ThemedText>
+
+          <Image
+            style={styles.winnerImage}
+            source={require('./assets/img/ann.png')}
+            placeholder="winner image"
+            contentFit="contain"
+            transition={1000}
+          />
+        </ThemedView>
+      }
+
     </ThemedView>
   )
 }
@@ -79,6 +100,16 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 22
+  },
+  winnerTitle: {
+    fontWeight: 'bold',
+    paddingVertical: 12,
+    marginTop: 25,
+    fontSize: 30
+  },
+  winnerImage: {
+    width: 200,
+    height: 150
   }
 })
 
